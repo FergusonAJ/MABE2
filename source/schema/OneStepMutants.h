402d5dc6ffbafa30baa86fa2ee66e67899057dcd
Austin Ferguson <ferguson.austin.j@gmail.com>
2020-08-19 10:39:21 -0400

    Change OneStepMutants to use best orgs

--- source/schema/OneStepMutants.h
+++ source/schema/OneStepMutants.h
@@ -23,6 +23,7 @@ namespace mabe {
     size_t update_step = 1;   ///< How many updates between operations?
     int min_mutation_idx = 0; ///< Where to start the mutations (defined by org.Mutate_Deterministic)
     int max_mutation_idx = 0; ///< Where to stop the mutations (exclusive)
+    std::string fitness_trait;
   public:
     OneStepMutants(mabe::MABE & control,
            const std::string & name="One Step Mutants",
@@ -31,10 +32,15 @@ namespace mabe {
            size_t _mutant_pop_id=1,
            size_t _update_step=1,
            int _min_mutation_idx=0,
-           int _max_mutation_idx=0)
-      : Module(control, name, desc), source_pop_id(_source_pop_id), mutant_pop_id(_mutant_pop_id),
-        update_step(_update_step),
-        min_mutation_idx(_min_mutation_idx), max_mutation_idx(_max_mutation_idx)
+           int _max_mutation_idx=0,
+           const std::string & _ftrait="fitness")
+      : Module(control, name, desc)
+        , source_pop_id(_source_pop_id)
+        , mutant_pop_id(_mutant_pop_id)
+        , update_step(_update_step)
+        , min_mutation_idx(_min_mutation_idx)
+        , max_mutation_idx(_max_mutation_idx)
+        , fitness_trait(_ftrait)
     {
     }
 
@@ -44,6 +50,7 @@ namespace mabe {
       LinkVar(update_step, "update_step", "How many updates between operations?");
       LinkVar(min_mutation_idx, "min_mutation_idx", "Where to start mutations (start as defined by the organism)?");
       LinkVar(max_mutation_idx, "max_mutation_idx", "Where to stop the mutations?");
+      LinkVar(fitness_trait, "fitness_trait", "Which trait should we use to rank organisms?");
     }
 
     void OnUpdate(size_t update) override {
@@ -51,7 +58,16 @@ namespace mabe {
         Population & source_pop = control.GetPopulation(source_pop_id);
         Population & mutant_pop = control.GetPopulation(mutant_pop_id);
         emp_assert(source_pop.GetSize() > 0);
-        const Organism & org = source_pop[0];
+        // Pick out the best-performing organism
+        double max_fitness = 0.0;
+        size_t max_idx = 0;
+        for(size_t org_idx = 0; org_idx < source_pop.GetSize(); ++org_idx){
+          if(source_pop[org_idx].GetVar<double>(fitness_trait) > max_fitness){
+            max_idx = org_idx;
+            max_fitness = source_pop[org_idx].GetVar<double>(fitness_trait);
+          }
+        }
+        const Organism & org = source_pop[max_idx];
         // Loop through the organisms (skipping any at the beginning that we need to) and
         // run Mutate() on each of them.
         size_t num_mutants = max_mutation_idx - min_mutation_idx;
