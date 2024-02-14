c854c9061944b4c6a580c4149075a7d0cbb110a4
Austin Ferguson <ferguson.austin.j@gmail.com>
2024-02-14 15:00:38 -0500

    Port over 2D grid evaluator and supporting files

--- source/modules.hpp
+++ source/modules.hpp
@@ -18,6 +18,7 @@
 #include "evaluate/games/EvalPatchHarvest.hpp"
 #include "evaluate/games/EvalDoors.hpp"
 #include "evaluate/games/EvalCues.hpp"
+#include "evaluate/static/Eval2DGrid.hpp"
 #include "evaluate/static/EvalCountBits.hpp"
 #include "evaluate/static/EvalDiagnostic.hpp"
 #include "evaluate/static/EvalMatchBits.hpp"
