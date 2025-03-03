b403137ceb6e0325b22c0ba825ec96f5b9f01081
Austin Ferguson <ferguson.austin.j@gmail.com>
2025-03-03 12:16:10 -0500

    Fix emp::String -based Bits construction

--- tests/unit/evaluate/static/EvalPacking.hpp
+++ tests/unit/evaluate/static/EvalPacking.hpp
@@ -19,25 +19,25 @@ TEST_CASE("EvalPacking_Evaluate", "[Evaluate/static]"){
     control.AddPopulation("fake pop"); 
     mabe::EvalPacking packing(control);
 
-    const emp::BitVector & bits = emp::BitVector("00");
-    const emp::BitVector & bits1 = emp::BitVector("000011110000");
-    const emp::BitVector & bits2 = emp::BitVector("11110000111100");
-    const emp::BitVector & bits3 = emp::BitVector("00000111011100");
-    const emp::BitVector & bits4 = emp::BitVector("111111111");
-    const emp::BitVector & bits5 = emp::BitVector(""); 
-    const emp::BitVector & bits6 = emp::BitVector("00011"); 
+    emp::BitVector bits("00");
+    emp::BitVector bits1("000011110000");
+    emp::BitVector bits2("11110000111100");
+    emp::BitVector bits3("00000111011100");
+    emp::BitVector bits4("111111111");
+    emp::BitVector bits5(""); 
+    emp::BitVector bits6("00011"); 
     
     // signature: Evaluate(bits, min_padding, package_size);
 
     // Package of ones is larger than what was expected 
     CHECK(packing.EvaluateOrg(bits, 2, 3) == 0);
     CHECK(packing.EvaluateOrg(bits4, 0, 20) == 0);
-    // Succesful package of ones 
-    CHECK(packing.EvaluateOrg(bits1, 4, 4) == 1);
-    // Succesful package of ones, no padding required if starting at beginning 
-    CHECK(packing.EvaluateOrg(bits2, 2, 4) == 2);
+    // Successful package of ones 
+    CHECK(packing.EvaluateOrg(bits1, 4, 4) == 1); 
+    // Successful package of ones, no padding required if starting at beginning 
+    CHECK(packing.EvaluateOrg(bits2, 2, 4) == 2); 
     // Interior padding (of the right length) can be reused
-    CHECK(packing.EvaluateOrg(bits3, 1, 3) == 2);
+    CHECK(packing.EvaluateOrg(bits3, 1, 3) == 2); 
     CHECK(packing.EvaluateOrg(bits3, 2, 3) == 0);
     // Works even with padding == 0
     CHECK(packing.EvaluateOrg(bits4, 0, 3) == 3);
@@ -47,7 +47,7 @@ TEST_CASE("EvalPacking_Evaluate", "[Evaluate/static]"){
     CHECK(packing.EvaluateOrg(bits1, 3, 4) == 1);
     CHECK(packing.EvaluateOrg(bits1, 2, 4) == 1);
     CHECK(packing.EvaluateOrg(bits1, 1, 4) == 1);
-    // Succesful package of ones, no padding required if package ends at end of bitstring 
+    // Successful package of ones, no padding required if package ends at end of bitstring 
     CHECK(packing.EvaluateOrg(bits6, 3, 2) == 1);
   }
 }
